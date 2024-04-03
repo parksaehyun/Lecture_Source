9aa995e57da5ae423075ebceca95e63cadb1ae4c
parksaehyun <psh666777@naver.com>
2024-04-03 19:26:09 +0900

    -

--- 2. 실무 프로젝트(P-1)/exam01/src/App.js
+++ 2. 실무 프로젝트(P-1)/exam01/src/App.js
@@ -1,6 +1,16 @@
-import MyComponent from "./components/MyComponent";
+//import MyComponent from './components/MyComponent2';
+//import Counter from "./components/Counter2";
+import Profile from "./components/Profile";
+
 const App = () => {
-  return <MyComponent   />;
+  return <Profile />
+  /*
+  return (
+    <MyComponent name={15}>
+      <h1>반값습니다.</h1>
+    </MyComponent>
+  );
+  */
 };
 
 export default App;
--- 2. 실무 프로젝트(P-1)/exam01/src/components/MyComponent.js
+++ 2. 실무 프로젝트(P-1)/exam01/src/components/MyComponent.js
@@ -1,22 +1,23 @@
-import ProTypes from 'prop-types';
+import PropTypes from 'prop-types';
 
-const MyComponent = ({ name, num }) => {
+const MyComponent = ({ name, num, children }) => {
   return (
     <>
       <div>안녕하세요!, 제 이름은 {name}입니다.</div>
       <div>좋아하는 숫자는 {num}입니다.</div>
+      {children}
     </>
   );
 };
 
 MyComponent.defaultProps = {
-  //기본값 설정
+  // 기본값 설정
   name: '기본 이름',
 };
 
 MyComponent.propTypes = {
-  name: ProTypes.string,
-  num: ProTypes.number,
+  name: PropTypes.string,
+  num: PropTypes.number.isRequired,
 };
 
 export default MyComponent;
