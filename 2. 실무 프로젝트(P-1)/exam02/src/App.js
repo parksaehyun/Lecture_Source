acb326a669ae9ffcd9ea07e3c7ca34c7c429ee38
parksaehyun <psh666777@naver.com>
2024-04-04 21:05:56 +0900

    리액트 및 자바스크립트 실습 이벤트 핸들링

---
+++ 2. 실무 프로젝트(P-1)/exam02/src/App.js
@@ -0,0 +1,12 @@
+//import JoinForm from './components/JoinForm';
+import Todos from './components/Todos';
+
+//const App = () => {
+//  return <JoinForm onClick={() => console.log('클릭!')} />;
+//};
+
+const App = () => {
+  return <Todos onClick={() => console.log('클릭!')} />;
+};
+
+export default App;
