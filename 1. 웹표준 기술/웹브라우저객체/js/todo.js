70557d7e4623556472dc76650d01eb968a7b9c7e
parksaehyun <psh666777@naver.com>
2024-04-08 17:58:02 +0900

    자바스크립트 localStorage, sessionStorage

--- 1. 웹표준 기술/웹브라우저객체/js/todo.js
+++ 1. 웹표준 기술/웹브라우저객체/js/todo.js
@@ -1,42 +1,74 @@
 const todo = {
-  /** 
-   * 스케줄 추가 
-   * 
-   * 
-   */
+    id: 1,
+    data: [], // 스케줄 데이터
+    init() {
+      // 저장 값 조회 -> 스케줄 완성
+      const jsonData = localStorage.getItem("todos");
+      const todos = jsonData ? JSON.parse(jsonData) : [];
   
-   add() {
-       const subject = frmRegist.subject.value;
-
-       if (!subject.trim()) {  //trim : 좌우 공백 제거
-           alert("할일을 입력하세요.");
-           return; // 할일을 입력하세요 팝업 뜨고 나오는 삭제리스트 안나오게 return은 더 이상 함수가 진행되지 않도록 하는 기능?
-       }
-
-       const liEl = document.createElement("li");
-       liEl.appendChild(document.createTextNode(subject));
-
-       const buttonEl = document.createElement("button");
-       buttonEl.appendChild(document.createTextNode("삭제"));
-       liEl.appendChild(buttonEl);
-
-       const itemsEl = document.querySelector(".items");
-       itemsEl.appendChild(liEl);
-       
-       //스케줄삭제
-       buttonEl.addEventListener("click", function() {
-           itemsEl.removeChild(liEl);
-       });
-
-       frmRegist.subject.value =""; //입력하고 바로 삭제
-       frmRegist.subject.focus(); //커서 깜빡깜빡
-       
-   },
-}; //객체 한개 더 만들기
-
-window.addEventListener("DOMContentLoaded", function() {
-   frmRegist.addEventListener("submit", function (e) {
-       e.preventDefault(); //기본동작 차단, 양식이 넘어가지 않고 머무르도록
-       todo.add(); //스케줄 추가 (바인딩?)
-   });
-});
+      const itemsEl = document.querySelector(".items");
+  
+      for (const item of todos) {
+        // Symbol.iterator / 반복자 패턴 / 반복이 필요한 객체
+        const liEl = this.getItem(item.title);
+        itemsEl.appendChild(liEl);
+      }
+    },
+    /**
+     * 스케줄 추가
+     *
+     */
+    add() {
+      const subject = frmRegist.subject.value;
+  
+      if (!subject.trim()) {
+        // 좌우 공백 제거
+        alert("할일을 입력하세요.");
+        return;
+      }
+  
+      const itemsEl = document.querySelector(".items");
+      const liEl = this.getItem(subject);
+      itemsEl.appendChild(liEl);
+  
+      frmRegist.subject.value = "";
+      frmRegist.subject.focus();
+  
+      const { data } = this;
+      let id = this.id++;
+      data.push({
+        id,
+        title: subject,
+      });
+  
+      this.save();
+    },
+    save() {
+      localStorage.setItem("todos", JSON.stringify(this.data));
+    },
+    getItem(subject) {
+      const liEl = document.createElement("li");
+      liEl.appendChild(document.createTextNode(subject));
+  
+      const buttonEl = document.createElement("button");
+      buttonEl.appendChild(document.createTextNode("삭제"));
+      liEl.appendChild(buttonEl);
+  
+      // 스케줄 삭제
+      buttonEl.addEventListener("click", function () {
+        const itemsEl = document.querySelector(".items");
+        itemsEl.removeChild(liEl);
+      });
+  
+      return liEl;
+    },
+  };
+  
+  window.addEventListener("DOMContentLoaded", function () {
+    todo.init(); // 데이터 조회 및 완성
+  
+    frmRegist.addEventListener("submit", function (e) {
+      e.preventDefault();
+      todo.add(); // 스케줄 추가
+    });
+  });
