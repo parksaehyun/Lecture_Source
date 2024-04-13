06a6b55fe7f25fd7f433f7667d45148f8c6f0be3
parksaehyun <psh666777@naver.com>
2024-04-13 17:29:26 +0900

    자바 배열 실습

---
+++ JAVA_practice/src/practice_day01_0413/Ex04.java
@@ -0,0 +1,12 @@
+package practice_day01_0413;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        int[] nums = {10, 20, 30, 40, 50, 60};
+        System.out.println(nums.length); // 공간의 갯수
+
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+    }
+}
