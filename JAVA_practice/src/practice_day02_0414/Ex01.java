d378146b4433b80f807f2c5761819b6f9de8a105
parksaehyun <psh666777@naver.com>
2024-04-14 19:31:43 +0900

    자바 함수 학습중

---
+++ JAVA_practice/src/practice_day02_0414/Ex01.java
@@ -0,0 +1,12 @@
+package practice_day02_0414;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Student s1 = new Student();
+        s1.id = 100;
+        s1.name = "학생1";
+        s1.subject = "과목1";
+
+        s1.study();
+    }
+}
