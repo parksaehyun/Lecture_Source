c233f44def18178d7fe0b5caff2ea179035327ed
parksaehyun <psh666777@naver.com>
2024-04-25 17:05:12 +0900

    자바 정규표현식 수업소스

--- 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex02.java
@@ -2,9 +2,20 @@ package exam02;
 
 public class Ex02 {
     public static void main(String[] args) {
-        //C c = new C();
-        //A a = c;
-        //B b = c;
+        C c = new C();
+
+
+
+        A a = c;
+
+
+        B b = c;
+
+
+
+        // 동질 이상
+
+
 
 
 
