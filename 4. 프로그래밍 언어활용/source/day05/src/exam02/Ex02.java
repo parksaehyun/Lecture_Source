7a685388bf693acaa07c96c33f48995afe343a9c
parksaehyun <psh666777@naver.com>
2024-04-25 18:20:23 +0900

    자바 다형성 복습

--- 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex02.java
+++ 4. 프로그래밍 언어활용/source/day05/src/exam02/Ex02.java
@@ -3,16 +3,17 @@ package exam02;
 public class Ex02 {
     public static void main(String[] args) {
         C c = new C();
-
-
-
         A a = c;
-
-
         B b = c;
 
 
 
+        System.out.printf("c 주소: %s%n", System.identityHashCode(c));
+        System.out.printf("b 주소: %s%n", System.identityHashCode(b));
+        System.out.printf("a 주소: %s%n", System.identityHashCode(a));
+
+        System.out.println(a == c);
+        System.out.println(b == c);
         // 동질 이상
 
 
