937c59728e81d8394b97f6ade21f3994878bdfc5
parksaehyun <psh666777@naver.com>
2024-05-01 09:07:44 +0900

    자바 내부클래스 복습중

--- 4. 프로그래밍 언어활용/source/day06/src/exam10/Ex01.java
+++ 4. 프로그래밍 언어활용/source/day06/src/exam10/Ex01.java
@@ -2,8 +2,8 @@ package exam10;
 
 public class Ex01 {
     public static void main(String[] args) {
-        Outer outer = new Outer();
-        Outer.Inner inner = outer.new Inner();
+        Outer outer = new Outer(); // 외부클래스 객체를 생성
+        Outer.Inner inner = outer.new Inner(); //Outer.Inner : 내부클래스의 자료형 // 내부 클래스 객체 생성
         inner.method();
         System.out.println(Outer.Inner.num3);
     }
