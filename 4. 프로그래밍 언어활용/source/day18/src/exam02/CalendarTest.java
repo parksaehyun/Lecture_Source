fd25dc1af0c12ffa86215d956027dcb2a08e2d6f
parksaehyun <psh666777@naver.com>
2024-05-09 16:06:34 +0900

    자바 날짜와 시간 수업소스

---
+++ 4. 프로그래밍 언어활용/source/day18/src/exam02/CalendarTest.java
@@ -0,0 +1,40 @@
+package exam02;
+
+import java.util.Scanner;
+
+public class CalendarTest {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            System.out.print("년 월 입력:");
+            String input = sc.nextLine(); // 2024 5
+
+            if (input.equals("exit")) {
+                System.out.println("종료 합니다.");
+                break;
+            }
+
+            try {
+                String[] data = input.split("\\s+");
+                int year = Integer.parseInt(data[0]);
+                int month = Integer.parseInt(data[1]);
+
+                System.out.printf("---- %d년 %d월 ----%n", year, month);
+                System.out.println("일  월  화  수  목  금  토");
+
+            } catch (Exception e) { // 숫자 형식 오류가 발생한 경우
+                System.out.println("입력 형식이 잘못되었습니다. 입력 예) 2024 5");
+            }
+        }
+    }
+
+    public static Object getCalendal(int year, int month){
+        /*
+        1. 매월의 1일이 어떤 요일에 시작하는지
+        2. 매월의 종료일이 무엇인지(30, 31, 28, 29)
+         */
+
+        return null;
+    }
+}
