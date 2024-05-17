6af0ce47ed3fbc53cdfe061521e52549c58fb3de
parksaehyun <psh666777@naver.com>
2024-05-17 16:55:06 +0900

    자바 쓰레드 수업소스

--- 4. 프로그래밍 언어활용/source/day22/src/exam01/Book.java
+++ 4. 프로그래밍 언어활용/source/day22/src/exam01/Book.java
@@ -3,9 +3,14 @@ package exam01;
 import java.io.Serializable;
 
 public class Book implements Serializable {
+    
+    private static final long serialVersionUID = 1L;
+
     private int isbn; //도서번호
     private String title; // 도서명
     private  String author; // 저자
+
+
 
     public Book(int isbn, String title, String author) {
         this.isbn = isbn;
