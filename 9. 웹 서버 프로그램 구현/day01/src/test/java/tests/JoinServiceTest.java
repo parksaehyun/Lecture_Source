e18bce7f8ac075c9d688d36272d2da9ba93945b3
parksaehyun <psh666777@naver.com>
2024-06-05 15:37:22 +0900

    TDD 방식 설계 수업소스 태깅과 필터링 수업소스

--- 9. 웹 서버 프로그램 구현/day01/src/test/java/tests/JoinServiceTest.java
+++ 9. 웹 서버 프로그램 구현/day01/src/test/java/tests/JoinServiceTest.java
@@ -96,5 +96,29 @@ public class JoinServiceTest {
         String message = thrown.getMessage();
         assertTrue(message.contains(keyword));
     }
+
+    @Test
+    @DisplayName("비밀번호와 비밀번호 확인 일치 테스트, 검증 실패시 ValidationException 발생")
+    void passwordMatchTest() {
+
+    }
+
+    @Test
+    @DisplayName("이메일 중복 여부 테스트, 검증 실패 시 DuplicatedMemberException 발생")
+    void duplicateEmailTest() {
+
+    }
+
+    @Test
+    @DisplayName("회원가입 완료 후 실제 데이터베이스에서 조회 되는지 검증")
+    void joinmemberExistTest() {
+        // 가입처리
+
+        // getData() 로 생성한 데이터의 email 항목으로 db 조회
+
+        // 조회한 회원 데이터 email과 getData()로 생성한 email이 일치하는지 체크
+        // assertEquals(...)
+
+    }
 }
 
