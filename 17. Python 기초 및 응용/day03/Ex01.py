6c7b067936f4e04da2e77f4cc4a66a5470f3e0e3
parksaehyun <psh666777@naver.com>
2024-07-09 17:05:01 +0900

    파이썬 자료형 수업소스

---
+++ 17. Python 기초 및 응용/day03/Ex01.py
@@ -0,0 +1,14 @@
+head = "Python"
+tail = "is fun!"
+print(head + tail)
+
+a = "python"
+print(a * 2)
+
+a = "Life is too short, You need Python"
+print(a[3]) # e
+print(a[-2]) # o
+print(a[0:4])
+print(a[10:])
+
+
