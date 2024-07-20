ec0f838f3c7f935b473f09bfcffbcb208c16e376
parksaehyun <psh666777@naver.com>
2024-07-20 22:15:39 +0900

    스프링 MVC 전체적으로 복습함(주석 추가)

--- 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
+++ 11. 프레임워크 프로그래밍/day05/src/main/java/org/choongang/config/MessageConfig.java
@@ -13,11 +13,11 @@ public class MessageConfig {
     @Bean
     public MessageSource messageSource() {
         // MessageSource : 인터페이스
-        ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
-        ms.setBasenames("messages.commons", "messages.validations", "messages.errors"); // 클래스 패스가 기준
-        ms.setDefaultEncoding("UTF-8");
+        ResourceBundleMessageSource ms = new ResourceBundleMessageSource(); //MessageSource 인터페이스를 구현하는 클래스, 프로퍼티 파일로부터 메시지를 로드
+        ms.setBasenames("messages.commons", "messages.validations", "messages.errors"); // 메시지 파일의 기본 이름을 설정// 클래스 패스가 기준
+        ms.setDefaultEncoding("UTF-8"); // 메시지 파일의 기본 인코딩을 UTF-8로 설정
         ms.setUseCodeAsDefaultMessage(true); // 메세지 코드가 없는 경우 코드로 메세지 대체
 
-        return ms;
+        return ms; // ResourceBundleMessageSource 객체를 반환하여, 이 객체가 스프링 컨텍스트의 MessageSource 빈으로 사용되도록 함
     }
 }
