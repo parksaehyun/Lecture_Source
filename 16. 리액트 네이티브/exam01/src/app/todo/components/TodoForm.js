266a4b800708a67adfac28750599f484e21494f9
parksaehyun <psh666777@naver.com>
2024-09-05 19:02:40 +0900

    리액트 훅 수업소스

--- 16. 리액트 네이티브/exam01/src/app/todo/components/TodoForm.js
+++ 16. 리액트 네이티브/exam01/src/app/todo/components/TodoForm.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import { IoMdRadioButtonOff, IoMdRadioButtonOn } from 'react-icons/io';
+import MessageBox from '@/app/commons/components/MessageBox';
 
-const TodoForm = ({ onSubmit }) => {
+const TodoForm = ({ onSubmit, onChange, onClick, form, titleRef, errors }) => {
   return (
     <form autoComplete="off" onSubmit={onSubmit}>
       <dl>
         <dt>할일</dt>
         <dd>
-          <input type="text" onChange={onChange} />
+          <input
+            type="text"
+            name="title"
+            value={form?.title ?? ''}
+            onChange={onChange}
+            ref={titleRef}
+          />
+          {errors?.title && <MessageBox>{errors.title}</MessageBox>}
         </dd>
       </dl>
       <dl>
         <dt>내용</dt>
         <dd>
-          <textarea></textarea>
+          <textarea
+            name="content"
+            onChange={onChange}
+            value={form?.content ?? ''}
+          ></textarea>
+          {errors?.content && <MessageBox>{errors.content}</MessageBox>}
         </dd>
       </dl>
       <dl>
         <dt>완료여부</dt>
         <dd>
-          <span>
-            <IoMdRadioButtonOff />
+          <span onClick={() => onClick(true)}>
+            {form?.done ? <IoMdRadioButtonOn /> : <IoMdRadioButtonOff />}
             완료
           </span>
-          <span>
-            <IoMdRadioButtonOff />
+          <span onClick={() => onClick(false)}>
+            {form?.done ? <IoMdRadioButtonOff /> : <IoMdRadioButtonOn />}
             미완료
           </span>
         </dd>
--- 16. 리액트 네이티브/exam01/src/app/todo/containers/TodoContainer.js
+++ 16. 리액트 네이티브/exam01/src/app/todo/containers/TodoContainer.js
@@ -1,16 +1,15 @@
 'use client';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { produce } from 'immer';
 import TodoForm from '../components/TodoForm';
 import TodoList from '../components/TodoList';
 
 const TodoContainer = () => {
-  const [items, setItems] = useState([
-    { id: 1, title: '할일1', done: true },
-    { id: 2, title: '할일2', done: false },
-    { id: 3, title: '할일3', done: false },
-  ]);
+  const [items, setItems] = useState([]);
   const [form, setForm] = useState({});
+  const [errors, setErrors] = useState({});
+
+  const titleRef = useRef();
 
   const onToggle = (id) => {
     /*
@@ -42,13 +41,66 @@ const TodoContainer = () => {
     setItems((items) => items.filter((item) => item.id !== id));
   };
 
+  const onChange = (e) => {
+    /**
+     * e.target
+     * //e.currentTarget
+     */
+
+    const name = e.target.name;
+    const value = e.target.value;
+    setForm((form) => ({ ...form, [name]: value }));
+  };
+
+  const onClick = (done) => {
+    setForm((form) => ({ ...form, done }));
+  };
+
   const onSubmit = (e) => {
     e.preventDefault(); // 양식 기본 동작 차단
+
+    // 검증
+    const _errors = {};
+    let hasErrors = false;
+
+    const requiredFields = {
+      title: '제목을 입력하세요.',
+      content: '내용을 입력하세요.',
+    };
+
+    for (const [field, message] of Object.entries(requiredFields)) {
+      if (!form[field] || !form[field].trim()) {
+        _errors[field] = _errors[field] ?? [];
+        _errors[field].push(message);
+        hasErrors = true;
+      }
+    }
+
+    setErrors(_errors);
+    if (hasErrors) {
+      // 검증 실패
+      return;
+    }
+
+    // 일정 등록 처리
+    const nextId = items ? Math.max(...items.map((item) => item.id)) + 1 : 1;
+    setItems((items) => items.concat({ ...form, id: nextId }));
+
+    // 양식 초기화
+    setForm({});
+    titleRef.current.focus();
   };
 
   return (
     <>
-      <TodoForm onSubmit={onSubmit} />
+      <TodoForm
+        onSubmit={onSubmit}
+        onChange={onChange}
+        onClick={onClick}
+        form={form}
+        titleRef={titleRef}
+        errors={errors}
+      />
       <TodoList items={items} onToggle={onToggle} onRemove={onRemove} />
     </>
   );
